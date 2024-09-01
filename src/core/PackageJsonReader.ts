ac59aec5e371c214ce72c065e8a121f88cdbf534
behemothoz <behemothoz@yandex.ru>
2024-09-01 19:04:44 +0300

    add PackageJsonReader and PackageJson classes

---
+++ src/core/PackageJsonReader.ts
@@ -0,0 +1,44 @@
+import * as vscode from 'vscode';
+
+import { fm } from './FileManager';
+
+interface PackageJsonFile {
+    name: string;
+    version: string;
+    dependencies?: Record<string, string>;
+    devDependencies?: Record<string, string>;
+}
+
+class PackageJson {
+    constructor(private packageJsonFile: PackageJsonFile) {}
+
+    get name() {
+        return this.packageJsonFile.name;
+    }
+
+    get version() {
+        return this.packageJsonFile.version;
+    }
+
+    get dependencies() {
+        return this.packageJsonFile.dependencies ?? {};
+    }
+
+    get devDependencies() {
+        return this.packageJsonFile.devDependencies ?? {};
+    }
+}
+
+export class PackageJsonReader {
+    static async read(packageJsonPath: vscode.Uri) {
+        try {
+            const fileContent = await fm.readFile(packageJsonPath);
+            const parsedPackageJson = JSON.parse(fileContent) as PackageJson;
+
+            return new PackageJson(parsedPackageJson);
+        } catch (error) {
+            console.error(`Error reading package.json file: ${error}`);
+            return null;
+        }
+    }
+}
