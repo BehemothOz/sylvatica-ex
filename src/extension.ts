8189dacca3708ddfb14ec089dfc07f0238f76a5d
behemothoz <behemothoz@yandex.ru>
2024-09-18 18:49:17 +0300

    remove unused code from extension file

--- src/extension.ts
+++ src/extension.ts
@@ -1,17 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import * as path from 'path';
-
-import { fm } from './core/FileManager';
-
-import { PackageManagerDetector } from './core/detecter';
-import { PackageJsonReader } from './core/PackageJsonReader';
-
-import { PackageBuilder } from './core/Package';
-import { comparison } from './core/comparison';
-
-import { WebPanel } from './core/panel/WebPanel';
 
 import { Sylvatica } from './sylvatica';
 
@@ -28,42 +17,12 @@ export function activate(context: vscode.ExtensionContext) {
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "helloworld-sample" is now active!');
 
-    const packageManagerDetector = new PackageManagerDetector();
-
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
     const disposable = vscode.commands.registerCommand('extension.helloWorld', async (file: vscode.Uri) => {
         // The code you place here will be executed every time your command is executed
         const sylvatica = new Sylvatica();
-        const directoryPath = path.dirname(file.fsPath);
-
-        const json = await PackageJsonReader.read(file);
-
-        const _ = new WebPanel(context);
-
-        comparison();
-
-        /*
-            Get installed dependencies version
-        */
-        const { dependencies } = json;
-        const moduleNames = Object.keys(dependencies);
-
-        for (const moduleName of moduleNames) {
-            const directoryPathAsUri = vscode.Uri.file(directoryPath);
-            // const localNodeModules = fm.joinPath(directoryPathAsUri, 'node_modules');
-            const packageJsonPath = fm.joinPath(directoryPathAsUri, 'node_modules', moduleName, 'package.json');
-
-            if (fm.exist(packageJsonPath)) {
-                try {
-                    const packageJsonModule = await PackageJsonReader.read(packageJsonPath);
-                    console.log(moduleName, packageJsonModule.version);
-                } catch (e) {
-                    console.log(e);
-                }
-            }
-        }
 
         // Display a message box to the user
         vscode.window.showInformationMessage('Hello World!');
