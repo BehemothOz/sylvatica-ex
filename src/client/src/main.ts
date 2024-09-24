ccd97440ddd3ec2dac4b18548a810085a873be60
behemothoz <behemothoz@yandex.ru>
2024-09-25 00:10:36 +0300

    mean web components

--- src/client/src/main.ts
+++ src/client/src/main.ts
@@ -1,4 +1,27 @@
 /*
     https://uit.stanford.edu/accessibility/concepts/tables/css-aria
     https://adrianroselli.com/2017/11/a-responsive-accessible-table.html
+
+    https://habr.com/ru/articles/546980/
+    https://learn.javascript.ru/webcomponents-intro
+*/
+
+interface Package {
+    name: string;
+    version: string;
+    lastVersion: string;
+    description: string;
+    homepage: string;
+}
+
+class Cell extends HTMLTableCellElement {
+    constructor() {
+        super();
+    }
+}
+
+customElements.define('cell', Cell);
+
+/*
+    package-info
 */
