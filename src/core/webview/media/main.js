7727a5d689e1af92e01f933b62c5e7f28af28464
behemothoz <behemothoz@yandex.ru>
2024-10-03 19:47:42 +0300

    simple ui

--- src/core/webview/media/main.js
+++ src/core/webview/media/main.js
@@ -11,11 +11,76 @@ window.addEventListener('message', event => {
         }
         case 'DEPENDENCIES': {
             // eslint-disable-next-line no-undef
-            console.log(event.data.data);
-            // eslint-disable-next-line no-undef
             const root = document.getElementById('root');
-            root.textContent = JSON.stringify(event.data.data, null, 2);
+
+            root.append(createTable(event.data.data));
             break;
         }
     }
 });
+
+const columns = [
+    {
+        title: 'Package',
+        key: 'name',
+    },
+    {
+        title: 'Current Version',
+        key: 'version',
+    },
+    {
+        title: 'Last Version',
+        key: 'lastVersion',
+    },
+    {
+        title: 'Description',
+        key: 'description',
+    },
+];
+
+function createTable(packages) {
+    // eslint-disable-next-line no-undef
+    const table = document.createElement('table');
+    // eslint-disable-next-line no-undef
+    const fragment = document.createDocumentFragment();
+
+    for (const package of packages) {
+        const row = createRow(package);
+        fragment.append(row);
+    }
+
+    table.append(createTableHeader());
+    table.append(fragment);
+
+    return table;
+}
+
+function createTableHeader() {
+    // eslint-disable-next-line no-undef
+    const header = document.createElement('tr');
+
+    for (const column of columns) {
+        // eslint-disable-next-line no-undef
+        const cell = document.createElement('th');
+        cell.textContent = column.title;
+
+        header.append(cell);
+    }
+
+    return header;
+}
+
+function createRow(package) {
+    // eslint-disable-next-line no-undef
+    const row = document.createElement('tr');
+
+    for (const column of columns) {
+        // eslint-disable-next-line no-undef
+        const cell = document.createElement('td');
+        cell.textContent = package[column.key];
+
+        row.append(cell);
+    }
+
+    return row;
+}
