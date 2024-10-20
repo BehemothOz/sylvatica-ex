1db18e4cce465b470736599c8ac24cd3003aa7e6
behemothoz <behemothoz@yandex.ru>
2024-10-20 20:17:50 +0300

    wb attribute

--- src/client/src/main.ts
+++ src/client/src/main.ts
@@ -41,6 +41,9 @@ class Spinner extends HTMLElement {
 
 class Icon extends HTMLElement {
     // ... todo
+    /*
+        <span class="icon"></span> + slot
+    */
 }
 
 /*
@@ -53,8 +56,8 @@ class Icon extends HTMLElement {
 class IconButton extends HTMLButtonElement {
     constructor() {
         super();
-        console.log(this);
         this.classList.add('button-icon');
+        console.log(this.getAttribute('color'));
     }
     // connectedCallback() {}
 }
