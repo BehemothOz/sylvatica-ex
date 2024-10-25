d173788db22073e351cd572effb55a610997c2f2
behemothoz <behemothoz@yandex.ru>
2024-10-25 17:10:03 +0300

    generate table

--- src/client/src/main.ts
+++ src/client/src/main.ts
@@ -6,29 +6,17 @@
     https://learn.javascript.ru/webcomponents-intro
 */
 
-interface Package {
-    name: string;
-    version: string;
-    lastVersion: string;
-    description: string;
-    homepage: string;
-}
-
-const data: Array<Package> = [
-    {
-        name: 'Axios',
-        version: '1.2.4',
-        lastVersion: '3.0.1',
-        description: 'Lorem ipsum dolor sit met, consectetur edit.',
-        homepage: 'https://www.google.com',
-    },
-];
+// const data: Array<Package> = [ --> package-info
+//     {
+//         name: 'Axios',
+//         version: '1.2.4',
+//         lastVersion: '3.0.1',
+//         description: 'Lorem ipsum dolor sit met, consectetur edit.',
+//         homepage: 'https://www.google.com',
+//     },
+// ];
 
 const dependenciesTable = document.getElementById('dependencies');
-
-/*
-    package-info
-*/
 
 const table = document.getElementById('table') as HTMLTableElement;
 
--- src/client/src/scripts/table.ts
+++ src/client/src/scripts/table.ts
@@ -1,6 +1,6 @@
-import { columns } from './columns';
+import { Column, Package } from './types';
 
-function generateHeader() {
+function createHeader(columns: Array<Column>) {
     const header = document.createElement('tr');
 
     for (const column of columns) {
@@ -20,4 +20,39 @@ function createCaption(title: string) {
     return caption;
 }
 
-function generateTable() {}
+function createRow(columns: Array<Column>, rowData: Package) {
+    const row = document.createElement('tr');
+
+    for (const column of columns) {
+        const cell = document.createElement('td');
+        const value = rowData[column.key];
+
+        cell.textContent = value;
+    }
+
+    return row;
+}
+
+function createRows(columns: Array<Column>, payload: Array<Package>) {
+    const fragment = document.createDocumentFragment();
+
+    for (const rowData of payload) {
+        const row = createRow(columns, rowData);
+        fragment.append(row);
+    }
+
+    return fragment;
+}
+
+export function generateTable(columns: Array<Column>, payload: Array<Package>) {
+    const table = document.createElement('table');
+
+    const caption = createCaption('Caption');
+    const header = createHeader(columns);
+
+    const rows = createRows(columns, payload);
+
+    table.append(caption);
+    table.append(header);
+    table.append(rows);
+}
