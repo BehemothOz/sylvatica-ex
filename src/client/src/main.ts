70e1626c99934610f7d6192be98e4d955a8843de
behemothoz <behemothoz@yandex.ru>
2024-10-27 18:03:12 +0300

    change colors for badge

--- src/client/src/main.ts
+++ src/client/src/main.ts
@@ -13,7 +13,7 @@ import { generateTable } from './scripts/table';
 
 const data = [
     {
-        name: 'Axios',
+        name: 'axios',
         diff: 'major',
         range: '^1.2.0',
         version: '1.2.4',
