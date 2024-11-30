ab7cc417171305f7b525c12e224bea1b5b451f3f
behemothoz <behemothoz@yandex.ru>
2024-11-30 20:59:53 +0300

    add new error

--- src/core/errors.ts
+++ src/core/errors.ts
@@ -1,11 +1,3 @@
-export class MissingNodeModulesError extends Error {
-    constructor(directoryPath: string) {
-        super(`Directory "node_modules" is missing in "${directoryPath}".`);
-
-        this.name = 'MissingNodeModulesError';
-    }
-}
-
 export class PackageJsonParseError extends Error {
     constructor(filePath: string, originalError?: Error) {
         super(`Failed to parse package.json at "${filePath}": ${originalError?.message}`);
@@ -15,5 +7,21 @@ export class PackageJsonParseError extends Error {
         if (originalError) {
             this.stack = originalError.stack;
         }
+    }
+}
+
+export class MissingNodeModulesError extends Error {
+    constructor(directoryPath: string) {
+        super(`Directory "node_modules" is missing in "${directoryPath}".`);
+
+        this.name = 'MissingNodeModulesError';
+    }
+}
+
+export class MissingPackageJsonError extends Error {
+    constructor(dependencyName: string, packageJsonPath: string) {
+        super(`Package.json for dependency "${dependencyName}" is missing at "${packageJsonPath}".`);
+
+        this.name = 'MissingPackageJsonError';
     }
 }
