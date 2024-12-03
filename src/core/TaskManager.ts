9fa9e6b1d9ecc8b2421b7ec886bb50183cab44a9
behemothoz <behemothoz@yandex.ru>
2024-12-03 21:29:15 +0300

    update task manager

--- src/core/TaskManager.ts
+++ src/core/TaskManager.ts
@@ -1,155 +1,37 @@
-export class QueueNode<T> {
-    value: T;
-    next: QueueNode<T> | null;
+import { Queue } from './queue';
 
-    constructor(value: T, next: QueueNode<T> | null = null) {
-        this.value = value;
-        this.next = next;
-    }
-}
-
-export class Queue<T> {
-    private head: QueueNode<T> | null;
-    private tail: QueueNode<T> | null;
-    private _size: number;
-
-    constructor() {
-        this.head = null;
-        this.tail = null;
-
-        this._size = 0;
-    }
-
-    get size() {
-        return this._size;
-    }
-
-    /**
-     * Add a new element to the end of the queue (the tail of the linked list).
-     */
-    enqueue(value: T) {
-        const node = new QueueNode(value);
-
-        if (this.tail) {
-            this.tail.next = node;
-            this.tail = node;
-        } else {
-            this.head = node;
-            this.tail = node;
-        }
-
-        this._size += 1;
-        return this;
-    }
-
-    /**
-     * Remove the element at the front of the queue (the head of the linked list).
-     * If the queue is empty, return null.
-     */
-    dequeue() {
-        if (this.head === null) {
-            return null;
-        }
-
-        const deletedNode = this.head;
-
-        if (this.head.next) {
-            this.head = this.head.next;
-        } else {
-            this.head = null;
-            this.tail = null;
-        }
-
-        this._size -= 1;
-        return deletedNode.value;
-    }
-
-    /**
-     * Read the element at the front of the queue without removing it.
-     */
-    peek() {
-        if (this.head === null) {
-            return null;
-        }
-
-        return this.head.value;
-    }
-
-    isEmpty() {
-        return this._size === 0;
-    }
-
-    /**
-     * @returns Returns an iterator over the values of the queue.
-     */
-    values(): IterableIterator<T> {
-        let current = this.head;
-
-        return {
-            [Symbol.iterator]() {
-                return this;
-            },
-            next: () => {
-                if (current === null) {
-                    return { done: true, value: undefined };
-                }
-
-                const result = {
-                    done: false,
-                    value: current.value,
-                };
-
-                current = current.next;
-
-                return result;
-            },
-        };
-    }
-
-    [Symbol.iterator]() {
-        return this.values();
-    }
-}
-
-// interface Task<> {
-//   retryCount: number;
-//   fn: () => Promise<TaskResult>;
-// }
-
-interface WaitingTask<T> {
-    originTask: () => Promise<T>;
-    resultPromise: Promise<T>;
-    retryCount: number;
+interface TaskItem<T> {
+    originalFn: () => Promise<T>;
+    resultPromise: Promise<T> | null;
+    /*
+        retryCount: number;
+    */
 }
 
 export class TaskManager<TaskResult = unknown> {
-    tasks: Queue<() => Promise<TaskResult>> = new Queue();
-    waitingTasks: Queue<WaitingTask<TaskResult>> = new Queue();
+    tasks: Queue<TaskItem<TaskResult>> = new Queue();
+    waitingTasks: Queue<TaskItem<TaskResult>> = new Queue();
 
-    taskIterator: IterableIterator<() => Promise<TaskResult>> | null = null;
+    taskIterator: IterableIterator<TaskItem<TaskResult>> | null = null;
 
     limit: number;
-    // 7
-    pending: number = 0;
-
     isRunning = false;
-
-    count: number = 0;
 
     constructor(limit: number = 4) {
         this.limit = limit;
     }
 
-    addTask(task: (() => Promise<TaskResult>) | Promise<TaskResult>) {
+    addTask(fn: () => Promise<TaskResult>) {
         if (this.isRunning) {
             throw new Error('The task execution process has already started');
         }
 
-        if (typeof task === 'function') {
-            this.tasks.enqueue(task);
-        } else {
-            this.tasks.enqueue(() => task);
-        }
+        const taskItem: TaskItem<TaskResult> = {
+            originalFn: fn,
+            resultPromise: null,
+        };
+
+        this.tasks.enqueue(taskItem);
     }
 
     async *run() {
@@ -169,12 +51,6 @@ export class TaskManager<TaskResult = unknown> {
                 yield await waitingTask.resultPromise;
             } catch (error) {
                 console.log(waitingTask);
-                if (waitingTask.retryCount === 3) {
-                    throw new Error('OOOPPPSSSS');
-                }
-
-                waitingTask.retryCount += 1;
-                this.waitingTasks.enqueue(waitingTask);
             }
         }
     }
@@ -189,36 +65,18 @@ export class TaskManager<TaskResult = unknown> {
         this.completeTask(task);
     }
 
-    private completeTask(taskPromiseResult: () => Promise<TaskResult>) {
-        const promise = taskPromiseResult()
-            .then((res) => {
-                this.pending -= 1;
+    private completeTask(task: TaskItem<TaskResult>) {
+        const promise = task.originalFn().finally(() => {
+            this.addTaskToComplete();
+        });
 
-                return res;
-            })
-            .catch((error) => {
-                console.log('error', error);
-                // this.pending += 1;
-                throw error;
-            })
-            .finally(() => {
-                this.addTaskToComplete();
-            });
-
-        const waitingTasksItem = {
-            originTask: taskPromiseResult,
-            resultPromise: promise,
-            retryCount: 0,
-        };
-
-        this.waitingTasks.enqueue(waitingTasksItem);
+        task.resultPromise = promise;
+        this.waitingTasks.enqueue(task);
     }
 
     private initializeTasks() {
         this.taskIterator = this.tasks.values();
-
         this.isRunning = true;
-        this.pending = this.tasks.size;
 
         for (let i = 0; i < this.limit; i += 1) {
             this.addTaskToComplete();
