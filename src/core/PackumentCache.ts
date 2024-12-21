275c42ee7effabee0ffdeba0247000e34b9bba16
behemothoz <behemothoz@yandex.ru>
2024-12-22 02:23:45 +0300

    fix packument cache

--- src/core/PackumentCache.ts
+++ src/core/PackumentCache.ts
@@ -1,18 +1,55 @@
+import * as vscode from 'vscode';
+import * as path from 'path';
+
 import { Cache } from './cache';
+import { Registry } from './registry';
+
 import { type PackumentInfo } from './Package';
+
+export class PackumentService {
+    caches: Map<string, PackumentCache> = new Map();
+
+    constructor() {}
+
+    async register(packageJsonPath: string) {
+        const packageJsonDirectory = vscode.Uri.file(path.dirname(packageJsonPath));
+        const currentRegistry = await Registry.build(packageJsonDirectory);
+
+        const packumentCache = new PackumentCache(currentRegistry);
+
+        this.caches.set(packageJsonPath, packumentCache);
+
+        return packumentCache;
+    }
+
+    delete(packageJsonPath: string) {
+        this.caches.delete(packageJsonPath);
+    }
+
+    clear() {
+        this.caches = new Map();
+    }
+}
 
 /*
     Packument is a special data format used in the npm (Node Package Manager) ecosystem to describe packages.
     It serves as a structured representation of information about a package, including its metadata and available versions.
 */
-export class PackumentCache extends Cache<PackumentInfo> {
-    async wrap(key: string) {
-        const value = this.get(key);
+class PackumentCache extends Cache<PackumentInfo> {
+    constructor(private registry: Registry) {
+        super();
+    }
+
+    async wrap(packageName: string) {
+        const value = this.get(packageName);
 
         if (value === undefined) {
-            const result = await sendRequest<PackumentInfo>(key);
+            const scope = packageName.split('/')[0];
+            const registryUrl = this.registry.getRegistryUrl(scope);
 
-            this.set(key, result);
+            const result = await sendRequest<PackumentInfo>(packageName, registryUrl);
+
+            this.set(packageName, result);
             return result;
         }
 
@@ -20,8 +57,14 @@ export class PackumentCache extends Cache<PackumentInfo> {
     }
 }
 
-async function sendRequest<T>(packageName: string): Promise<T> {
-    const response = await fetch(`https://registry.npmjs.org/${packageName}/latest`);
+async function sendRequest<T>(packageName: string, registryUrl: string): Promise<T> {
+    const packageUrl = new URL(`${encodeURIComponent(packageName).replace(/^%40/, '@')}/latest`, registryUrl);
+
+    const response = await fetch(packageUrl, {
+        headers: {
+            accept: 'application/vnd.npm.install-v1+json; q=1.0, application/json; q=0.8, */*',
+        },
+    });
     const result = (await response.json()) as T;
     console.log('result -->', result);
     if (!response.ok) {
