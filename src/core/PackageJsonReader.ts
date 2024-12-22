26bea5f6abd8d1ed0be75291ac1036c8cf876a03
behemothoz <behemothoz@yandex.ru>
2024-12-22 15:17:02 +0300

    add js-doc and error

--- src/core/PackageJsonReader.ts
+++ src/core/PackageJsonReader.ts
@@ -1,12 +1,26 @@
 import * as vscode from 'vscode';
 
 import { fm } from './FileManager';
-import { PackageJsonParseError } from './errors';
 
+/**
+ *  Represents the required structure of a `package.json` file.
+ */
 interface PackageJsonFile {
+    /**
+     * Name of the package.
+     */
     name: string;
+    /**
+     * Version of the package.
+     */
     version: string;
+    /**
+     * Dependencies of the package, if any.
+     */
     dependencies?: Record<string, string>;
+    /**
+     * Development dependencies of the package, if any.
+     */
     devDependencies?: Record<string, string>;
 }
 
@@ -30,7 +44,13 @@ export class PackageJson {
     }
 }
 
+/**
+ *  A class for reading and parsing `package.json` files.
+ */
 export class PackageJsonReader {
+    /**
+     *  Reads and parses a `package.json` file from the given URI.
+     */
     static async read(packageJsonPath: vscode.Uri) {
         try {
             const fileContent = await fm.readFile(packageJsonPath);
@@ -39,6 +59,18 @@ export class PackageJsonReader {
             return new PackageJson(parsedPackageJson);
         } catch (error) {
             throw new PackageJsonParseError(packageJsonPath.fsPath);
+        }
+    }
+}
+
+export class PackageJsonParseError extends Error {
+    constructor(filePath: string, originalError?: Error) {
+        super(`Failed to parse package.json at "${filePath}": ${originalError?.message}`);
+
+        this.name = 'PackageJsonParseError';
+
+        if (originalError) {
+            this.stack = originalError.stack;
         }
     }
 }
