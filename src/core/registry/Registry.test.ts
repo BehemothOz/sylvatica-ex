75c6a13f979af9cb7abbd229f9b17413ef82a59e
behemothoz <behemothoz@yandex.ru>
2025-01-15 21:56:26 +0300

    ns base

--- src/core/registry/Registry.test.ts
+++ src/core/registry/Registry.test.ts
@@ -1,9 +1,19 @@
 import mock from 'mock-fs';
 
+function createRcFileContent() {
+    const rows = [
+        'registry=https://registry.npmjs.org/authToken=your-auth-token',
+        'registry=https://registry.npmjs.org/authToken=your-auth-token',
+        'registry=https://registry.npmjs.org/authToken=your-auth-token',
+    ];
+
+    return rows.join('\n');
+}
+
 describe('Storage', () => {
     beforeEach(() => {
         mock({
-            'fake-file': 'file contents',
+            'fake-file': createRcFileContent(),
         });
     });
 
